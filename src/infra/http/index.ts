fff43e86844dcb405b091386feeaed384746a72c
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-06-30 20:24:42 -0300

    feat(infra): http status code util class

--- src/infra/http/index.ts
+++ src/infra/http/index.ts
@@ -3,11 +3,13 @@ import { IMiddleware } from "./models/IMiddleware";
 import { IPaginationOptions } from "./models/IPaginationOptions";
 import { IPaginationResponse } from "./models/IPaginationResponse";
 import { IResponseMessage } from "./models/IResponseMessage";
+import { HttpStatus } from "./utils/HttpStatus";
 
 export {
   app,
   IResponseMessage,
   IMiddleware,
+  HttpStatus,
   IPaginationOptions,
   IPaginationResponse,
 };
---
+++ src/infra/http/utils/HttpStatus.ts
@@ -0,0 +1,15 @@
+class HttpStatus {
+  public static GENERAL_STATUS = 200;
+
+  public static CREATED = 201;
+
+  public static BAD_REQUEST = 400;
+
+  public static UNAUTHORIZED = 401;
+
+  public static NOT_FOUD = 404;
+
+  public static GENERAL_ERROR = 500;
+}
+
+export { HttpStatus };
