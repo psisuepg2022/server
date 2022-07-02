475190e0d4cc68dc61b885fc8684225a0275c481
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-02 13:27:10 -0300

    refactor(helpers): using toNumber function to input validation

--- src/helpers/pagination.ts
+++ src/helpers/pagination.ts
@@ -3,32 +3,36 @@ import { AppError } from "@handlers/error/AppError";
 import { IPaginationOptions } from "@infra/http";
 
 import { env } from "./env";
+import { toNumber } from "./toNumber";
 
 const pagination = ({ size, page }: IPaginationOptions): [number, number] => {
   const take = ((): number => {
     if (size) {
-      const converted = Number(size);
-      if (Number.isNaN(converted))
-        throw new AppError("BAD_REQUEST", i18n.__("ErrorQueryTypecasting"));
+      const converted = toNumber({
+        value: size,
+        error: new AppError("BAD_REQUEST", i18n.__("ErrorQueryTypecasting")),
+      });
 
       return converted;
     }
 
-    const sizeDefault = Number(env("PAGE_SIZE_DEFAULT"));
-    if (!sizeDefault || Number.isNaN(sizeDefault))
-      throw new AppError(
+    const sizeDefault = toNumber({
+      value: env("PAGE_SIZE_DEFAULT"),
+      error: new AppError(
         "INTERNAL_SERVER_ERROR",
         i18n.__("ErrorEnvPageSizeDefault")
-      );
+      ),
+    });
 
     return sizeDefault;
   })();
 
   const skip = ((): number => {
     if (page) {
-      const converted = Number(page);
-      if (Number.isNaN(converted))
-        throw new AppError("BAD_REQUEST", i18n.__("ErrorQueryTypecasting"));
+      const converted = toNumber({
+        value: page,
+        error: new AppError("BAD_REQUEST", i18n.__("ErrorQueryTypecasting")),
+      });
       return converted * take;
     }
 
