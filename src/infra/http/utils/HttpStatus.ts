12843a9463ff5ab4a35c02fcfb907a325edd487b
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-02 13:06:37 -0300

    refactor(infra): change http status to enum

--- src/infra/http/utils/HttpStatus.ts
+++ src/infra/http/utils/HttpStatus.ts
@@ -1,15 +1,10 @@
-class HttpStatus {
-  public static GENERAL_STATUS = 200;
-
-  public static CREATED = 201;
-
-  public static BAD_REQUEST = 400;
-
-  public static UNAUTHORIZED = 401;
-
-  public static NOT_FOUD = 404;
-
-  public static GENERAL_ERROR = 500;
+enum HttpStatus {
+  GENERAL_STATUS = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUD = 404,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
 export { HttpStatus };
