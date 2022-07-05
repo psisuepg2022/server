2fcb9399af84bc63a2ac091c05af5cedd61a1624
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-05 20:56:22 -0300

    feat(infra): securiy - xss & hpp

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,4 +1,9 @@
-type keys = "PORT" | "PAGE_SIZE_DEFAULT" | "PASSWORD_HASH_SALT" | "SUPPORT_ID";
+type keys =
+  | "PORT"
+  | "PAGE_SIZE_DEFAULT"
+  | "PASSWORD_HASH_SALT"
+  | "SUPPORT_ID"
+  | "LIST_ALLOWED_ORIGINS";
 
 const env = (key: keys): string | undefined => {
   if (!key) return undefined;
--- src/infra/http/app/index.ts
+++ src/infra/http/app/index.ts
@@ -1,23 +1,29 @@
 import "reflect-metadata";
 import "express-async-errors";
+import "@containers/index";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
-
-import "@containers/index";
+import hpp from "hpp";
 
 import { RoutesPrefix } from "@common/RoutesPrefix";
+import { env } from "@helpers/env";
 import {
   errorHandlerMiddleware,
   internationalizationMiddleware,
 } from "@middlewares/index";
 import { routes } from "@routes/index";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const xss = require("xss-clean");
+
 const app = express();
 
 app.use(helmet());
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: env("LIST_ALLOWED_ORIGINS")?.split(" ") }));
+app.use(express.json({ limit: "10kb" }));
+app.use(xss());
+app.use(hpp());
 app.use(internationalizationMiddleware);
 app.use(RoutesPrefix.API, routes);
 app.use(errorHandlerMiddleware);
