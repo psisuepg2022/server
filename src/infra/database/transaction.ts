6eb5d4350a541ee86cf913b9104d0d1e1266791f
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-05 14:17:55 -0300

    feat(infra): database transaction

---
+++ src/infra/database/transaction.ts
@@ -0,0 +1,9 @@
+import { PrismaPromise, UnwrapTuple } from "@prisma/client";
+
+import { prismaClient } from "./client";
+
+const transaction = async <P extends PrismaPromise<any>[]>(
+  events: [...P]
+): Promise<UnwrapTuple<P>> => prismaClient.$transaction(events);
+
+export { transaction };
