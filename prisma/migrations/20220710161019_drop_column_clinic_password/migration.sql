b4cf0de58af969ee9faa974d1729e8abb15cf6b2
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-10 13:14:25 -0300

    chore(db/schema): drop password column of clinic table

---
+++ prisma/migrations/20220710161019_drop_column_clinic_password/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `clinic` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "clinic" DROP COLUMN "password";
