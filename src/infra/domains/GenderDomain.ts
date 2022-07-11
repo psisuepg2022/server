aee1c9fb334995fe6efe119f33ac93f9abafc242
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-11 09:50:01 -0300

    feat(infra): domains - gender & marital status

---
+++ src/infra/domains/GenderDomain.ts
@@ -0,0 +1,9 @@
+enum GenderDomain {
+  MALE = 1,
+  FEMALE = 2,
+  TRANSGENDER = 3,
+  NON_BINARY = 4,
+  PREFER_NOT_TO_RESPOND = 5,
+}
+
+export { GenderDomain };
---
+++ src/infra/domains/MaritalStatusDomain.ts
@@ -0,0 +1,8 @@
+enum MaritalStatusDomain {
+  MARRIED = 1,
+  SINGLE = 2,
+  DIVORCED = 3,
+  WIDOWED = 4,
+}
+
+export { MaritalStatusDomain };
