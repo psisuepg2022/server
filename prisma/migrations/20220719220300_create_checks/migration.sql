38b78bc9b4c9e5bd149cde9f7cabc8c7a758e12b
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-19 23:14:45 -0300

    chore(db/schema): new checks

--- prisma/migrations/20220719220300_create_checks/migration.sql
+++ prisma/migrations/20220719220300_create_checks/migration.sql
@@ -8,3 +8,9 @@ ALTER TABLE "paciente" ADD CONSTRAINT paciente_check_sexo CHECK ("sexo" IN (1, 2
 
 -- CreateCheck
 ALTER TABLE "paciente" ADD CONSTRAINT paciente_check_estado_civil CHECK ("estado_civil" IN (1, 2, 3, 4));
+
+-- CreateCheck
+ALTER TABLE "agenda_semanal" ADD CONSTRAINT agenda_semanal_check_dia_semana CHECK ("dia_semana" IN (1, 2, 3, 4, 5, 6, 7));
+
+-- CreateCheck
+ALTER TABLE "agenda_semanal" ADD CONSTRAINT agenda_semanal_check_horario CHECK ("horario_inicio" < "horario_fim");
