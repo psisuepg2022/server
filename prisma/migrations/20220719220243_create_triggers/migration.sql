2d5e5f49ea455261915e5ccfe5124336bc8964b6
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-07-20 10:35:53 -0300

    chore(db/schema): create trigger - appointment date validation

--- prisma/migrations/20220719220243_create_triggers/migration.sql
+++ prisma/migrations/20220719220243_create_triggers/migration.sql
@@ -20,3 +20,37 @@ CREATE TRIGGER trg_gerar_codigo_usuario
 BEFORE INSERT ON "usuario"
 FOR EACH ROW
 EXECUTE PROCEDURE gerar_codigo_usuario();
+
+-- CreateTrigger
+CREATE FUNCTION previnir_consultas_mesmo_horario() RETURNS TRIGGER
+AS $$
+  DECLARE 
+    DURACAO_CONSULTA INTEGER;
+    ID_CONSULTA_EXISTENTE CHARACTER VARYING(36) := NULL;
+BEGIN
+  SELECT p.duracao_base INTO DURACAO_CONSULTA
+  FROM profissional p 
+  WHERE p.id = NEW.id_profissional;
+
+  SELECT c.id INTO ID_CONSULTA_EXISTENTE
+  FROM consulta c
+  WHERE 
+    c.id_profissional = NEW.id_profissional 
+    AND c.situacao <> 4
+    AND new.data_agendamento > c.data_agendamento
+    AND new.data_agendamento < (c.data_agendamento + (DURACAO_CONSULTA* interval '1 minute'))
+  LIMIT 1;
+
+  IF (coalesce(ID_CONSULTA_EXISTENTE, '') <> '') THEN
+    raise exception 'Já existe uma consulta marcada não-concluída para este profissional no horário informado.';
+  END IF;
+
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_previnir_consultas_mesmo_horario
+BEFORE INSERT ON "consulta"
+FOR EACH ROW
+EXECUTE PROCEDURE previnir_consultas_mesmo_horario();
