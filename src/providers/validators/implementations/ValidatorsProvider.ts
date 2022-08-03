e1cf915b7bcdcf5307591f63562e97ef4e76fa32
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-08-02 21:06:11 -0300

    refactor(providers): validators provider - exec regex

--- src/providers/validators/implementations/ValidatorsProvider.ts
+++ src/providers/validators/implementations/ValidatorsProvider.ts
@@ -11,13 +11,10 @@ class ValidatorsProvider implements IValidatorsProvider {
   time = (time: string): boolean => /^[0-9]{2}(?::)[0-9]{2}$/.test(time);
 
   zipCode = (zipCode: string): boolean =>
-    !"/^[0-9]{5}(?:-)[0-9]{3}/".match(zipCode);
+    /^[0-9]{5}(?:-)[0-9]{3}/.test(zipCode);
 
   contactNumber = (contactNumber: string): boolean =>
-    !(
-      contactNumber.length !== 15 ||
-      "/^(?:()[0-9]{2}(?:))s?[0-9]{4,5}(?:-)[0-9]{4}$/".match(contactNumber)
-    );
+    /^(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/.test(contactNumber);
 }
 
 export { ValidatorsProvider };
