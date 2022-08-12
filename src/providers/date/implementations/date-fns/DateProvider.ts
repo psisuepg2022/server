478c2c25926b9ed68716261107a0dc79e8ffbdeb
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-08-12 10:43:06 -0300

    feat(repositories): add aux methods on date provider

--- src/providers/date/implementations/date-fns/DateProvider.ts
+++ src/providers/date/implementations/date-fns/DateProvider.ts
@@ -1,9 +1,29 @@
+import { isAfter, format, isBefore } from "date-fns";
 import { zonedTimeToUtc } from "date-fns-tz";
 
 import { IDateProvider } from "@providers/date/models/IDateProvider";
 
 class DateProvider implements IDateProvider {
+  getUTCDate = (date: string, time?: string): Date =>
+    zonedTimeToUtc(new Date(`${date}${time ? ` ${time}` : ""}`), "");
+
   now = (): Date => zonedTimeToUtc(new Date().toISOString(), "");
+
+  getTodayWithoutTime = (): Date =>
+    zonedTimeToUtc(`${format(this.now(), "yyyy-MM-dd")}`, "");
+
+  isAfter = (date: Date, toCompare: Date): boolean => isAfter(date, toCompare);
+
+  isBefore = (date: Date, toCompare: Date): boolean =>
+    isBefore(date, toCompare);
+
+  minuteToMilli = (minute: number): number => minute * 6000;
+
+  differenceInMillis = (end: Date, start: Date): number =>
+    end.getTime() - start.getTime();
+
+  time2date = (time: string, date = "2022-08-12"): Date =>
+    zonedTimeToUtc(`${date} ${time}:00`, "");
 }
 
 export { DateProvider };
--- src/providers/date/models/IDateProvider.ts
+++ src/providers/date/models/IDateProvider.ts
@@ -1,5 +1,12 @@
 interface IDateProvider {
   now(): Date;
+  getTodayWithoutTime(): Date;
+  getUTCDate(date: string, time?: string): Date;
+  isAfter(date: Date, toCompare: Date): boolean;
+  isBefore(date: Date, toCompare: Date): boolean;
+  minuteToMilli(minute: number): number;
+  differenceInMillis(end: Date, start: Date): number;
+  time2date(time: string, date?: string): Date;
 }
 
 export { IDateProvider };
