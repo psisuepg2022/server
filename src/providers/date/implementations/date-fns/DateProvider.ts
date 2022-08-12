b4ed9e3a3c6b93d9ddbcd89fd48ab96bc566877a
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-08-12 11:44:07 -0300

    refactor(provider): date provider - timezone handling

--- src/providers/date/implementations/date-fns/DateProvider.ts
+++ src/providers/date/implementations/date-fns/DateProvider.ts
@@ -1,16 +1,22 @@
-import { isAfter, format, isBefore } from "date-fns";
+import ptBR, { isAfter, format, isBefore } from "date-fns";
 import { zonedTimeToUtc } from "date-fns-tz";
 
 import { IDateProvider } from "@providers/date/models/IDateProvider";
 
 class DateProvider implements IDateProvider {
   getUTCDate = (date: string, time?: string): Date =>
-    zonedTimeToUtc(new Date(`${date}${time ? ` ${time}` : ""}`), "");
+    zonedTimeToUtc(
+      new Date(`${date}${time ? ` ${time}:00.000Z` : ""}`),
+      "America/Sao_Paulo",
+      { locale: ptBR }
+    );
 
   now = (): Date => zonedTimeToUtc(new Date().toISOString(), "");
 
   getTodayWithoutTime = (): Date =>
-    zonedTimeToUtc(`${format(this.now(), "yyyy-MM-dd")}`, "");
+    zonedTimeToUtc(`${format(this.now(), "yyyy-MM-dd")}`, "America/Sao_Paulo", {
+      locale: ptBR,
+    });
 
   isAfter = (date: Date, toCompare: Date): boolean => isAfter(date, toCompare);
 
@@ -23,7 +29,9 @@ class DateProvider implements IDateProvider {
     end.getTime() - start.getTime();
 
   time2date = (time: string, date = "2022-08-12"): Date =>
-    zonedTimeToUtc(`${date} ${time}:00`, "");
+    zonedTimeToUtc(`${date} ${time}:00.000Z`, "America/Sao_Paulo", {
+      locale: ptBR,
+    });
 }
 
 export { DateProvider };
