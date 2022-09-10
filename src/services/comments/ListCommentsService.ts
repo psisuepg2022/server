6a41197a11642ee902024c641da7ee20f9a369c2
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-10 12:58:32 -0300

    feat(services): list comments - validations

--- src/services/comments/ListCommentsService.ts
+++ src/services/comments/ListCommentsService.ts
@@ -1,11 +1,41 @@
+import i18n from "i18n";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
 import { IPaginationOptions, IPaginationResponse } from "@infra/http";
 import { ListCommentsRequestModel } from "@models/dto/comments/ListCommentsRequestModel";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
+@injectable()
 class ListCommentsService {
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider
+  ) {}
+
   public async execute(
-    _: ListCommentsRequestModel,
+    { patientId, professionalId }: ListCommentsRequestModel,
     __: IPaginationOptions
   ): Promise<IPaginationResponse<ListCommentsResponseModel>> {
+    if (stringIsNullOrEmpty(patientId))
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorUserIDRequired", ["paciente"])
+      );
+
+    if (stringIsNullOrEmpty(professionalId))
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorUserIDRequired", ["profissional"])
+      );
+
+    if (
+      !this.uniqueIdentifierProvider.isValid(professionalId) ||
+      !this.uniqueIdentifierProvider.isValid(patientId)
+    )
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
+
     return { items: [], totalItems: 0 };
   }
 }
