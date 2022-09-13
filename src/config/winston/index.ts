8967f5083fc521183c40f9fc93cceb8735057b28
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-12 23:50:27 -0300

    refactor(config): timestamp log config - timezone

--- src/config/winston/index.ts
+++ src/config/winston/index.ts
@@ -7,14 +7,23 @@ const directory = path.join(__dirname, "..", "..", "..", "log", "db");
 const options: LoggerOptions = {
   format: format.combine(
     format.label({ label: "LOG" }),
-    format.timestamp(),
+    format.timestamp({
+      format: () => {
+        return new Date().toLocaleString("pt-BR", {
+          timeZone: "America/Sao_Paulo",
+        });
+      },
+    }),
     format.printf(
       ({ level, message, label, timestamp }) =>
         `${timestamp} [${label}] ${level}: ${message}`
     )
   ),
   transports: [
-    new DailyRotateFile({ filename: `${directory}/log-%DATE%.log` }),
+    new DailyRotateFile({
+      filename: `${directory}/log_%DATE%.log`,
+      datePattern: "YYYY-MM-DD",
+    }),
   ],
 };
 
