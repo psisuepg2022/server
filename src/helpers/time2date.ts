a5d22e341fd0c9eb486442b53b0fd0ff3b0eec2c
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-15 11:25:48 -0300

    refactor(helpers): rm unused func - time2date

--- src/helpers/time2date.ts
+++
@@ -1,7 +0,0 @@
-const time2date = (time: string, [year, month, date] = [2022, 6, 27]): Date => {
-  const [hour, minutes] = time.split(":");
-
-  return new Date(Date.UTC(year, month, date, Number(hour), Number(minutes)));
-};
-
-export { time2date };
