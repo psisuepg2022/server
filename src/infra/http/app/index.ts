0eb6914689cc98f7bffa8f682eb8968d483d68a2
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-15 13:29:55 -0300

    feat(infra): logs - serving static files with express route

--- src/infra/http/app/index.ts
+++ src/infra/http/app/index.ts
@@ -5,12 +5,14 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import hpp from "hpp";
+import path from "path";
 
 import { RoutesPrefix } from "@common/RoutesPrefix";
 import { env } from "@helpers/env";
 import {
   errorHandlerMiddleware,
   internationalizationMiddleware,
+  isSupportMiddleware,
 } from "@middlewares/index";
 import { routes } from "@routes/index";
 
@@ -26,6 +28,11 @@ app.use(xss());
 app.use(hpp());
 app.use(internationalizationMiddleware);
 app.use(RoutesPrefix.API, routes);
+app.use(
+  "/logs/:support",
+  isSupportMiddleware,
+  express.static(path.join(__dirname, "..", "..", "..", "..", "log", "db"))
+);
 app.use(errorHandlerMiddleware);
 
 process.on("SIGTERM", () => {
