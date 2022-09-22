c2a1e90f67ab1c428bb4b53dc3d37539df034617
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-21 23:08:25 -0300

    feat(services): get appointment - validations

--- src/services/appointment/GetAppointmentService.ts
+++ src/services/appointment/GetAppointmentService.ts
@@ -1,10 +1,37 @@
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
 import { AppointmentModel } from "@models/domain/AppointmentModel";
 import { GetAppointmentCommentsRequestModel } from "@models/dto/comments/GetAppointmentCommentsRequestModel";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
+@injectable()
 class GetAppointmentService {
-  public async execute(
-    _: GetAppointmentCommentsRequestModel
-  ): Promise<AppointmentModel> {
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider
+  ) {}
+
+  public async execute({
+    appointmentId,
+    professionalId,
+  }: GetAppointmentCommentsRequestModel): Promise<AppointmentModel> {
+    if (stringIsNullOrEmpty(professionalId))
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorUserIDRequired", ["profissional"])
+      );
+
+    if (stringIsNullOrEmpty(appointmentId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorAppointmentIdRequired"));
+
+    if (
+      !this.uniqueIdentifierProvider.isValid(professionalId) ||
+      !this.uniqueIdentifierProvider.isValid(appointmentId)
+    )
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
+
     return {} as AppointmentModel;
   }
 }
