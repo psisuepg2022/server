2071e4f9f33f5cb9425efa7b52899a4591eb6ef5
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-09-26 20:56:47 -0300

    feat(services): configure professional - validations

--- src/services/professional/ConfigureProfessionalService.ts
+++ src/services/professional/ConfigureProfessionalService.ts
@@ -2,16 +2,20 @@ import i18n from "i18n";
 import { inject, injectable } from "tsyringe";
 
 import { AppError } from "@handlers/error/AppError";
+import { getEnumDescription } from "@helpers/getEnumDescription";
 import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
 import { toNumber } from "@helpers/toNumber";
 import { transaction } from "@infra/database/transaction";
+import { DaysOfTheWeek } from "@infra/domains";
 import { WeeklyScheduleLockModel } from "@models/domain/WeeklyScheduleLockModel";
 import { ConfigureProfessionalRequestModel } from "@models/dto/professional/ConfigureProfessionalRequestModel";
 import { ConfigureWeeklyScheduleLocksRequestModel } from "@models/dto/weeklySchedule/ConfigureWeeklyScheduleRequestModel";
+import { CreateWeeklyScheduleLockRequestModel } from "@models/dto/weeklySchedule/CreateWeeklyScheduleLockRequestModel";
 import { PrismaPromise, WeeklySchedule } from "@prisma/client";
 import { IDateProvider } from "@providers/date";
 import { IPasswordProvider } from "@providers/password";
 import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
+import { IValidatorsProvider } from "@providers/validators";
 import { IProfessionalRepository } from "@repositories/professional";
 
 @injectable()
@@ -24,8 +28,95 @@ class ConfigureProfessionalService {
     @inject("DateProvider")
     private dateProvider: IDateProvider,
     @inject("PasswordProvider")
-    private passwordProvider: IPasswordProvider
+    private passwordProvider: IPasswordProvider,
+    @inject("ValidatorsProvider")
+    private validatorsProvider: IValidatorsProvider
   ) {}
+
+  private validateInterval = (
+    start: string,
+    end: string,
+    index: number,
+    dayOfTheWeek: string,
+    type: "weekly_schedule" | "lock"
+  ): void => {
+    this.validateTime(start, index, dayOfTheWeek, type, "Start");
+    this.validateTime(end, index, dayOfTheWeek, type, "End");
+
+    if (this.dateProvider.time2date(start) > this.dateProvider.time2date(end))
+      throw new AppError(
+        "BAD_REQUEST",
+        type === "lock"
+          ? i18n.__mf("ErrorConfigureProfessionalWeeklyLockIntervalInvalid", [
+              index + 1,
+              dayOfTheWeek,
+            ])
+          : i18n.__mf("ErrorConfigureProfessionalWeeklyIntervalInvalid", [
+              dayOfTheWeek,
+            ])
+      );
+  };
+
+  private validateTime = (
+    time: string,
+    index: number,
+    dayOfTheWeek: string,
+    type: "weekly_schedule" | "lock",
+    label: "Start" | "End"
+  ): void => {
+    if (stringIsNullOrEmpty(time))
+      throw new AppError(
+        "BAD_REQUEST",
+        type === "lock"
+          ? i18n.__mf(
+              `ErrorConfigureProfessionalWeeklyLock${label}TimeRequired`,
+              [index + 1, dayOfTheWeek]
+            )
+          : i18n.__mf(`ErrorConfigureProfessionalWeekly${label}TimeRequired`, [
+              dayOfTheWeek,
+            ])
+      );
+
+    if (!this.validatorsProvider.time(time))
+      throw new AppError(
+        "BAD_REQUEST",
+        type === "lock"
+          ? i18n.__mf(
+              `ErrorConfigureProfessionalWeeklyLock${label}TimeInvalid`,
+              [index + 1, dayOfTheWeek]
+            )
+          : i18n.__mf(`ErrorConfigureProfessionalWeekly${label}TimeInvalid`, [
+              dayOfTheWeek,
+            ])
+      );
+  };
+
+  private validateIntervalOutOfBounds = (
+    start: Date,
+    end: Date,
+    toCompare: number,
+    dayOfTheWeek: string,
+    index: number,
+    type: "weekly_schedule" | "lock"
+  ): void => {
+    if (
+      (this.dateProvider.differenceInMillis(end, start) /
+        this.dateProvider.minuteToMilli(toCompare)) %
+      2
+    )
+      throw new AppError(
+        "BAD_REQUEST",
+        type === "lock"
+          ? i18n.__mf(
+              "ErrorConfigureProfessionalWeeklyLockIntervalOutOfBounds",
+              [index + 1, dayOfTheWeek, toCompare]
+            )
+          : i18n.__mf("ErrorConfigureProfessionalWeeklyIntervalOutOfBounds", [
+              dayOfTheWeek,
+              toCompare,
+            ])
+      );
+  };
 
   public async execute({
     userId,
@@ -89,7 +180,7 @@ class ConfigureProfessionalService {
     if (!this.passwordProvider.hasStrength(newPassword))
       throw new AppError("BAD_REQUEST", i18n.__("ErrorPasswordToWeak"));
 
-    if (!weeklySchedule)
+    if (!weeklySchedule || !Array.isArray(weeklySchedule))
       throw new AppError(
         "BAD_REQUEST",
         i18n.__("ErrorConfigureProfessionalWeeklyScheduleRequired")
@@ -98,14 +189,117 @@ class ConfigureProfessionalService {
     const createWeeklyScheduleOperations: PrismaPromise<WeeklySchedule>[] = [];
     const createLocksOperations: PrismaPromise<WeeklyScheduleLockModel>[] = [];
 
-    if (Array.isArray(weeklySchedule) && weeklySchedule.length > 0)
+    if (weeklySchedule.length > 7)
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__("ErrorConfigureProfessionalWeeklyMoreThanOneWeek")
+      );
+
+    if (weeklySchedule.length > 0)
       weeklySchedule.forEach(
         (
-          item: ConfigureWeeklyScheduleLocksRequestModel,
-          index: number
+          {
+            startTime,
+            endTime,
+            dayOfTheWeek,
+            locks,
+          }: ConfigureWeeklyScheduleLocksRequestModel,
+          indexWeeklySchedule: number
         ): void => {
-          console.log(item, index);
-          createLocksOperations.push();
+          if (
+            weeklySchedule.filter((item) => item.dayOfTheWeek === dayOfTheWeek)
+              .length !== 1
+          )
+            throw new AppError(
+              "BAD_REQUEST",
+              i18n.__("ErrorConfigureProfessionalDuplicateDayOfTheWeek")
+            );
+
+          if (stringIsNullOrEmpty(dayOfTheWeek))
+            throw new AppError(
+              "BAD_REQUEST",
+              i18n.__mf("ErrorConfigureProfessionalDayOfTheWeekRequired", [
+                indexWeeklySchedule + 1,
+              ])
+            );
+
+          const dayOfTheWeekConverted = toNumber({
+            value: dayOfTheWeek,
+            error: new AppError(
+              "BAD_REQUEST",
+              i18n.__mf("ErrorConfigureProfessionalDayOfTheWeekInvalid", [
+                indexWeeklySchedule + 1,
+              ])
+            ),
+          });
+
+          if (!(dayOfTheWeekConverted in DaysOfTheWeek))
+            throw new AppError(
+              "BAD_REQUEST",
+              i18n.__mf("ErrorConfigureProfessionalDayOfTheWeekInvalid", [
+                indexWeeklySchedule + 1,
+              ])
+            );
+
+          const descriptionDayOfTheWeek = getEnumDescription(
+            "DAYS_OF_THE_WEEK",
+            DaysOfTheWeek[dayOfTheWeekConverted]
+          );
+
+          this.validateInterval(
+            startTime,
+            endTime,
+            indexWeeklySchedule,
+            descriptionDayOfTheWeek,
+            "weekly_schedule"
+          );
+
+          const endTimeConverted = this.dateProvider.time2date(endTime);
+          const startTimeConverted = this.dateProvider.time2date(startTime);
+
+          this.validateIntervalOutOfBounds(
+            startTimeConverted,
+            endTimeConverted,
+            baseDurationConverted,
+            descriptionDayOfTheWeek,
+            indexWeeklySchedule,
+            "weekly_schedule"
+          );
+
+          if (Array.isArray(locks) && locks.length > 0)
+            locks.forEach(
+              (
+                lock: CreateWeeklyScheduleLockRequestModel,
+                indexLocks: number
+              ): void => {
+                this.validateInterval(
+                  lock.startTime,
+                  lock.endTime,
+                  indexLocks,
+                  descriptionDayOfTheWeek,
+                  "lock"
+                );
+
+                const lockStartTimeConverted = this.dateProvider.time2date(
+                  lock.startTime
+                );
+                const lockEndTimeConverted = this.dateProvider.time2date(
+                  lock.endTime
+                );
+
+                this.validateIntervalOutOfBounds(
+                  lockStartTimeConverted,
+                  lockEndTimeConverted,
+                  baseDurationConverted,
+                  descriptionDayOfTheWeek,
+                  indexLocks,
+                  "lock"
+                );
+
+                createLocksOperations.push();
+              }
+            );
+
           createWeeklyScheduleOperations.push();
         }
       );
@@ -119,8 +313,6 @@ class ConfigureProfessionalService {
         "NOT_FOUND",
         i18n.__mf("ErrorUserIDNotFound", ["profissional"])
       );
-
-    console.log("");
   }
 }
 
