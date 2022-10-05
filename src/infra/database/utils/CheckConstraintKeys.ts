155dda752a887e5893c1e765dcad48607c8c6fbb
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-10-04 22:44:00 -0300

    feat(infra): database checks & triggers - map key -> message

--- src/infra/database/utils/CheckConstraintKeys.ts
+++ src/infra/database/utils/CheckConstraintKeys.ts
@@ -1,3 +1,15 @@
-enum CheckConstraintKeys {}
+enum CheckConstraintKeys {
+  "pessoa_check_nascimento" = "ErrorFutureBirthDate",
+  "paciente_check_genero" = "ErrorValueOutOfGenderDomain",
+  "paciente_check_estado_civil" = "ErrorValueOutOfMaritalStatusDomain",
+  "agenda_semanal_check_dia_semana" = "ErrorWeekScheduleDayOfTheWeekInvalid",
+  "agenda_semanal_check_horario" = "ErrorWeeklyScheduleInvalidInterval",
+  "restricoes_horario_check_horario" = "ErrorScheduleLockIntervalInvalid",
+  "restricoes_horario_check_data" = "ErrorScheduleLockPastDate",
+  "restricoes_agenda_semanal_check_horario" = "ErrorWeeklyScheduleInvalidInterval",
+  "consulta_check_situacao" = "ErrorValueOutOfAppointmentStatusDomain",
+  "consulta_check_data_agendamento" = "ErrorAppointmentPastDate",
+  "profissional_duracao_base_check" = "ErrorBaseDurationLessThanZero",
+}
 
 export { CheckConstraintKeys };
--- src/infra/database/utils/TriggerKeys.ts
+++ src/infra/database/utils/TriggerKeys.ts
@@ -1,3 +1,8 @@
-enum TriggerKeys {}
+enum TriggerKeys {
+  "TRIGGER_RAISE_EXCEPTION_0" = "ErrorAppointmentAlreadyExists_NoParams",
+  "TRIGGER_RAISE_EXCEPTION_1" = "ErrorAppointmentDontHaveTimeWithoutLock",
+  "TRIGGER_RAISE_EXCEPTION_2" = "ErrorAppointmentDontHaveTimeWithoutLock",
+  "TRIGGER_RAISE_EXCEPTION_3" = "ErrorAppointmentDateWithScheduleLock_NoParams",
+}
 
 export { TriggerKeys };
