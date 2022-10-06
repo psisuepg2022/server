20dbfb36cad582be1129c681b48e4e3d0ff991ab
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-10-06 14:12:33 -0300

    chore(db/schema): rm appointmenteDate check

---
+++ prisma/migrations/20221006171108_rm_check_appointment_date/migration.sql
@@ -0,0 +1,5 @@
+-- This is an empty migration.
+
+-- DropCheck
+ALTER TABLE "consulta" DROP CONSTRAINT IF EXISTS consulta_check_data_agendamento;
+  
--- src/infra/database/utils/CheckConstraintKeys.ts
+++ src/infra/database/utils/CheckConstraintKeys.ts
@@ -8,7 +8,6 @@ enum CheckConstraintKeys {
   "restricoes_horario_check_data" = "ErrorScheduleLockPastDate",
   "restricoes_agenda_semanal_check_horario" = "ErrorWeeklyScheduleInvalidInterval",
   "consulta_check_situacao" = "ErrorValueOutOfAppointmentStatusDomain",
-  "consulta_check_data_agendamento" = "ErrorAppointmentPastDate",
   "profissional_duracao_base_check" = "ErrorBaseDurationLessThanZero",
 }
 
