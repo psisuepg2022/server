b5e004c80830626abe4388980b4da421815d78ad
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-10-14 18:17:51 -0300

    chore(scripts): db backup/restore by environment

--- scripts/db/backup_database.sh
+++ scripts/db/backup_database.sh
@@ -2,5 +2,4 @@
 
 cd backup/db && \
 sudo docker exec -t "$DEV_CONTAINER" \
-pg_dump -c -h localhost -U postgres \
-"$DEV_DB_NAME" > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+pg_dump "$DATABASE_URL" > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
--- scripts/db/restore_database.sh
+++ scripts/db/restore_database.sh
@@ -2,4 +2,4 @@
 
 sudo docker exec \
 -i "$DEV_CONTAINER" /bin/bash \
--c "psql -U postgres ${DEV_DB_NAME}" < "backup/db/$1"
+-c "psql ${DATABASE_URL}" < "backup/db/$1"
